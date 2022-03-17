c1c787dd1147bb251c8d8ea9592181ba409c6eae
Sam Gubernikoff <sgubernikoff@gmail.com>
2022-03-17 09:38:01 -0400

    commit tree 5a3e34de8e9a2f277e7ba8d458728f0ab5504243

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,11 @@
+require 'pry'
 class Person
-  # your code here
+
+def initialize array 
+  array.each do |key, value|
+    self.class.attr_accessor(key)
+    self.send("#{key}=", value)
+  end
+end
+
 end
